b2d377fad029e50e0c034bc5910a414c1e231a22
Chris Ferdian <chris.ferdian@aia.com>
2019-05-08 20:26:11 +0700

    Initial Commit

---
+++ KitaMath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KitaMath
+//
+//  Created by AIA-Chris on 08/05/19.
+//  Copyright © 2019 Chrizers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
